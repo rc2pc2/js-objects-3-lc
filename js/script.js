7d7e5ee43c96fb1ef485fbedbb89f143f80cefb9
rc2pc2 <petriccarcc@gmail.com>
2023-11-27 12:07:28 +0100

    cars snack

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const shoppingList = [
-    'patate',
-    'burger vegano',
-    'carne finta',
-    'latte di castagne',
-    'castagne',
-    'cioccolata'
-];
+// const shoppingList = [
+//     'patate',
+//     'burger vegano',
+//     'carne finta',
+//     'latte di castagne',
+//     'castagne',
+//     'cioccolata'
+// ];
 
 // for (let index = 0; index < shoppingList.length; index++) {
 //     const element = shoppingList[index];
@@ -26,57 +26,146 @@ const shoppingList = [
 
 
 
-const students = [
-    {
-        name: 'Gino',
-        average: 3.5,
-    },
+// const students = [
+//     {
+//         name: 'Gino',
+//         average: 3.5,
+//     },
 
-    {
-        name: 'Ginetta',
-        average: 4.5,
-    },
+//     {
+//         name: 'Ginetta',
+//         average: 4.5,
+//     },
 
-    {
-        name: 'Lorenzina',
-        average: 27,
-    },
+//     {
+//         name: 'Lorenzina',
+//         average: 27,
+//     },
 
-    {
-        name: 'Ugo',
-        average: 22,
-    },
+//     {
+//         name: 'Ugo',
+//         average: 22,
+//     },
 
-    {
-        name: 'Giorgetta',
-        average: 18.5,
-    },
+//     {
+//         name: 'Giorgetta',
+//         average: 18.5,
+//     },
 
-    {
-        name: 'Caligola',
-        average: 30,
-    },
+//     {
+//         name: 'Caligola',
+//         average: 30,
+//     },
 
+//     {
+//         name: 'Amina',
+//         average: 15,
+//     }
+// ];
+// const filteredStudents = students.filter((student) => {
+//     if ( student.average < 18 ){
+//         return true;
+//     }
+// });
+
+// const newArray = [];
+// for (let index = 0; index < students.length; index++) {
+//     const element = students[index];
+//     if ( element.average < 18){
+//         newArray.push(element);
+//     }
+// }
+
+// console.log(filteredStudents);
+// console.log(newArray);
+// console.log(students);
+
+
+const cars = [
     {
-        name: 'Amina',
-        average: 15,
-    }
+        brand: 'Mercedes',
+        model: 'class s',
+        engine: 'gasoline',
+    },
+    {
+        brand: 'Audi',
+        model: 'R8',
+        engine: 'gasoline',
+    },
+    {
+        brand: 'Mercedes',
+        model: 'eqs',
+        engine: ' electric',
+    },
+    {
+        brand: 'Mercedes',
+        model: 'gt 63 s amg',
+        engine: 'gasoline',
+    },
+    {
+        brand: 'BMW',
+        model: 'i8',
+        engine: 'electric',
+    },
+    {
+        brand: 'Ferrari',
+        model: 'sf-90 stradale',
+        engine: 'electric gasoline',
+    },
+    {
+        brand: 'Lamborghini',
+        model: 'Urus',
+        engine: 'gasoline',
+    },
+    {
+        brand: 'BMW',
+        model: 'X5',
+        engine: 'diesel',
+    },
+    {
+        brand: 'Nissan',
+        model: 'Boh',
+        engine: 'diesel',
+    },
+    {
+        brand: 'Toyota',
+        model: 'Yougo',
+        engine: 'diesel',
+    },
+    {
+        brand: 'Audi',
+        model: 'RSQ8',
+        engine: 'gasoline',
+    },
+    {
+        brand: 'Mercedes',
+        model: 'class eqc',
+        engine: 'electric',
+    },
 ];
-const filteredStudents = students.filter((student) => {
-    if ( student.average < 18 ){
+
+const gasCars = cars.filter((singleCar) =>{
+    if (singleCar.engine.includes('gasoline')){
         return true;
     }
 });
 
-const newArray = [];
-for (let index = 0; index < students.length; index++) {
-    const element = students[index];
-    if ( element.average < 18){
-        newArray.push(element);
+console.log(gasCars);
+
+// ? const dieselCars = cars.filter((singleCar) => singleCar.engine.includes('diesel'));
+
+const dieselCars = cars.filter((singleCar) =>{
+    if (singleCar.engine.includes('diesel')){
+        return true;
     }
-}
+});
 
+console.log(dieselCars);
 
-console.log(filteredStudents);
-console.log(newArray);
-console.log(students);
+const otherCars = cars.filter((singleCar) =>{
+    if (singleCar.engine != 'diesel' && singleCar.engine != 'gasoline')
+        return true;
+});
+
+console.log(otherCars);
+
